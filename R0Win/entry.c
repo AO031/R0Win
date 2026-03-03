746bc575b2e827399f1c76724e88d830f1955db9
AO031 <3034089412@qq.com>
2026-03-03 22:11:52 +0800

    feat:今天简单学习了进程相关操作

--- R0Win/entry.c
+++ R0Win/entry.c
@@ -1,11 +1,11 @@
 #include <ntifs.h>
 
 VOID DriverUnload(PDRIVER_OBJECT driverObject) {
-	DbgPrint("[Winter] DriverUnload\n");
+	DbgPrint("[W] DriverUnload\n");
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT driverObject, PUNICODE_STRING regPath) {
 	driverObject->DriverUnload = DriverUnload;
-	DbgPrint("[Winter] DriverEntry\n");
+	DbgPrint("[W] DriverEntry\n");
 	return STATUS_SUCCESS; 
 }
